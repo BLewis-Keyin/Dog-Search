b9ade4ade284a1ed9769e14a8595d4da7d5ba0cc
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2023-06-25 01:48:15 +0000

    Changes to be committed: 	modified:   my-app/src/App.js 	renamed:    my-app/src/images/dog-bone-png-dog-bone-transparent-images-pngio-24 1.png -> my-app/src/images/DogBone.png 	modified:   my-app/src/index.css 	modified:   my-app/src/index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+import DogBone from "./images/DogBone.png"
 
 const App = () => {
-  const [selectedBreed, setSelectedBreed] = useState('');
-  const [numImages, setNumImages] = useState('');
+  const [selectedBreed, setSelectedBreed] = useState("");
+  const [numImages, setNumImages] = useState("");
 
   const handleSearch = (breed, numImages) => {
     setSelectedBreed(breed);
@@ -13,11 +14,26 @@ const App = () => {
   
   return (
     <div>
-      <SearchBar onSearch={handleSearch} onNumImagesChange={setNumImages} />
-      <ImageList selectedBreed={selectedBreed} numImages={numImages} />
-    </div>
+      <div className="main">
+        <div className="group">
+          <h1 className="text-wrapper">Dog Search</h1>
+                      <img
+            className="dog-bone-png-dog"
+            alt="Dog bone png dog"
+            src={DogBone}
+          />
+        </div>
+        <SearchBar onSearch={handleSearch} onNumImagesChange={setNumImages}/>
+        <div className="label">
+          <div className="div-wrapper">
+            <div className="text-wrapper">DogSearchLLC©</div>
+          </div>
+        </div>
+          </div>
+          <ImageList selectedBreed={selectedBreed} numImages={numImages} />
+        </div>
+      
   );
 };
 
 export default App;
-
