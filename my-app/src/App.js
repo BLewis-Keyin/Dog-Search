1a3927637c2ce8a6243cd4794dca79d6a138c292
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2023-06-29 02:44:18 +0000

    Bug fixes, polishing
    Fixes applied:
    
    Fixed issue that caused the app to be pushed down by about 20 pixels when loading images
    
    When less then 5 images are loaded, the images will no longer be weirdly spaced . They will now float to the left and grow from left to right properly .
    
    Reduced the size of the scrollbar in the breed selection menu
    
    Fixed the clipping with the imagelist div that caused scrolling when only 2 rows of images are present.
    
    Changes to be committed:
    	modified:   my-app/src/App.css
    	modified:   my-app/src/App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,6 @@ import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 import DogBone from "./images/DogBone.png"
 import Rectangle from "./images/rectangle-3.svg"
-import Background from "./images/image_background.png"
 const App = () => {
   const [selectedBreed, setSelectedBreed] = useState("");
   const [numImages, setNumImages] = useState("");
@@ -17,7 +16,7 @@ const App = () => {
     <div>
       <div className="main">
         <div className="background">
-          <ImageList selectedBreed={selectedBreed} numImages={numImages} />
+          
           <div className="group">
             <h1 className="head-text-wrapper">Dog Search</h1>
             <img
@@ -33,7 +32,8 @@ const App = () => {
             src={Rectangle}
           />
         </div>
-        <SearchBar onSearch={handleSearch} onNumImagesChange={setNumImages}/>
+          <SearchBar onSearch={handleSearch} onNumImagesChange={setNumImages} />
+          <ImageList selectedBreed={selectedBreed} numImages={numImages} />
         <div className="label">
           <div className="div-wrapper">
             <div className="text-wrapper">DogSearchLLC©</div>
